b3b7d8ffa018313bc2a952e05856baf251a6aaef
Maximilian Köhler <maximilian.koehler@ruhr-uni-bochum.de>
2021-01-02 13:56:08 +0100

    getnodes and getelements, latter only returns connectivity as of now, need to change to a Vector{<:AbstractCell}

--- src/JuAFEMGmsh.jl
+++ src/JuAFEMGmsh.jl
@@ -1,5 +1,21 @@
 module JuAFEMGmsh
 
-greet() = print("Hello World!")
+using gmsh
+using JuAFEM
 
-end # module
+function getnodes()
+    nodeid, nodes = JuAFEMGmsh.gmsh.model.mesh.getNodes()
+    dim = Int64(JuAFEMGmsh.gmsh.model.getDimension()) #Int64 otherwise julia crashes
+    return [Node(Vec{dim}(nodes[i:i+(dim-1)])) for i in 1:3:length(nodes)]
+end
+
+function getelements(dim)
+    elementtypes, elementtags, nodetags = JuAFEMGmsh.gmsh.model.mesh.getElements(dim)
+    @assert length(elementtypes)==1 "only one element type per mesh is supported"
+    elementname, dim, order, numnodes, localnodecoord, numprimarynodes = JuAFEMGmsh.gmsh.model.mesh.getElementProperties(2) 
+    nodetags = convert(Array{Array{Int64,1},1}, nodetags)
+    connectivity = [Tuple(nodetags[1][i:i+(numnodes-1)]) for i in 1:numnodes:length(nodetags[1])]
+    return connectivity
+end
+
+end
---
+++ test/devtests.jl
@@ -0,0 +1,22 @@
+using JuAFEMGmsh
+
+JuAFEMGmsh.gmsh.initialize()
+JuAFEMGmsh.gmsh.option.setNumber("General.Terminal", 1)
+JuAFEMGmsh.gmsh.model.add("t1")
+lc = 1e-2
+JuAFEMGmsh.gmsh.model.geo.addPoint(0, 0, 0, lc, 1)
+JuAFEMGmsh.gmsh.model.geo.addPoint(.1, 0,  0, lc, 2)
+JuAFEMGmsh.gmsh.model.geo.addPoint(.1, .3, 0, lc, 3)
+JuAFEMGmsh.gmsh.model.geo.addPoint(0, .3, 0, lc, 4)
+JuAFEMGmsh.gmsh.model.geo.addLine(1, 2, 1)
+JuAFEMGmsh.gmsh.model.geo.addLine(3, 2, 2)
+JuAFEMGmsh.gmsh.model.geo.addLine(3, 4, 3)
+JuAFEMGmsh.gmsh.model.geo.addLine(4, 1, 4)
+JuAFEMGmsh.gmsh.model.geo.addCurveLoop([4, 1, -2, 3], 1)
+JuAFEMGmsh.gmsh.model.geo.addPlaneSurface([1], 1)
+JuAFEMGmsh.gmsh.model.addPhysicalGroup(0, [1, 2], 1)
+JuAFEMGmsh.gmsh.model.addPhysicalGroup(1, [1, 2], 2)
+JuAFEMGmsh.gmsh.model.addPhysicalGroup(2, [1], 6)
+JuAFEMGmsh.gmsh.model.setPhysicalName(2, 6, "My surface")
+JuAFEMGmsh.gmsh.model.geo.synchronize()
+JuAFEMGmsh.gmsh.model.mesh.generate(2)
