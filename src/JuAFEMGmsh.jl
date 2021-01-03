682c0931a02c27593bdc51ef5699e22d49e805eb
Maximilian Köhler <maximilian.koehler@ruhr-uni-bochum.de>
2021-01-03 17:35:02 +0100

    first working version

--- src/JuAFEMGmsh.jl
+++ src/JuAFEMGmsh.jl
@@ -14,26 +14,26 @@ const gmshtojuafemcell = Dict("Line 2" => Line,
                               "Hexahedron 8" => Hexahedron,
                               "Hexahedron 20" => QuadraticHexahedron)
 
-function getnodes()
+function tonodes()
     nodeid, nodes = gmsh.model.mesh.getNodes()
     dim = Int64(gmsh.model.getDimension()) #Int64 otherwise julia crashes
     return [Node(Vec{dim}(nodes[i:i+(dim-1)])) for i in 1:3:length(nodes)]
 end
 
-function getelements(dim)
-    elementtypes, elementtags, nodetags = gmsh.model.mesh.getElements(dim)
+function toelements(dim::Int)
+    elementtypes, elementtags, nodetags = gmsh.model.mesh.getElements(dim, -1)
     @assert length(elementtypes)==1 "only one element type per mesh is supported"
     elementname, dim, order, numnodes, localnodecoord, numprimarynodes = gmsh.model.mesh.getElementProperties(elementtypes[1]) 
     nodetags = convert(Array{Array{Int64,1},1}, nodetags)[1]
     juafemcell = gmshtojuafemcell[elementname]
     elements = [juafemcell(Tuple(nodetags[i:i+(numnodes-1)])) for i in 1:numnodes:length(nodetags)]
-    return elements
+    return elements, convert(Vector{Vector{Int64}},elementtags)[1]
 end
 
-function getboundary(dim)
+function toboundary(dim::Int)
     boundarydict = Dict{String, Vector}()
     boundaries = gmsh.model.getPhysicalGroups(dim) 
-    for (tag,boundary) in enumerate(boundaries)
+    for boundary in boundaries
         physicaltag = boundary[2]
         name = gmsh.model.getPhysicalName(dim, physicaltag)
         boundaryentities = gmsh.model.getEntitiesForPhysicalGroup(dim, physicaltag)
@@ -41,7 +41,7 @@ function getboundary(dim)
         for entity in boundaryentities
             boundarytypes, boundarytags, boundarynodetags = gmsh.model.mesh.getElements(dim, entity)
             _, _, _, numnodes, _, _= gmsh.model.mesh.getElementProperties(boundarytypes[1]) 
-            boundarynodetags = convert(Array{Array{Int64,1},1}, boundarynodetags)[1]
+            boundarynodetags = convert(Vector{Vector{Int64}}, boundarynodetags)[1]
             append!(boundaryconnectivity,[Tuple(boundarynodetags[i:i+(numnodes-1)]) for i in 1:numnodes:length(boundarynodetags)])
         end
         boundarydict[name] = boundaryconnectivity
@@ -49,11 +49,11 @@ function getboundary(dim)
     return boundarydict
 end
 
-function getfaceset(boundarydict::Dict{String,Vector}, elements::Vector{<:JuAFEM.AbstractCell})
+function tofacesets(boundarydict::Dict{String,Vector}, elements::Vector{<:JuAFEM.AbstractCell})
     faces = JuAFEM.faces.(elements)
-    facesets = Dict{String,Set{Tuple{Int64,Int64}}}()
+    facesets = Dict{String,Set{Tuple{Int,Int}}}()
     for (boundaryname, boundaryfaces) in boundarydict
-        facesettuple = Set{Tuple{Int64,Int64}}()
+        facesettuple = Set{Tuple{Int,Int}}()
         for boundaryface in boundaryfaces
             for (eleidx, elefaces) in enumerate(faces)
                 if boundaryface in elefaces
@@ -67,7 +67,53 @@ function getfaceset(boundarydict::Dict{String,Vector}, elements::Vector{<:JuAFEM
     return facesets
 end
 
+function tocellsets(dim::Int, global_elementtags::Vector{Int})
+    cellsets = Dict{String, Set{Int}}()
+    physicalgroups = gmsh.model.getPhysicalGroups(dim)
+    for (_,physicaltag) in physicalgroups 
+        gmshname = gmsh.model.getPhysicalName(dim,physicaltag)
+        isempty(gmshname) ? (name = "$physicaltag") : (name = gmshname)
+        cellsetentities = gmsh.model.getEntitiesForPhysicalGroup(dim,physicaltag)
+        cellsetelements = Set{Int}()
+        for entity in cellsetentities
+            _, elementtags, _ = gmsh.model.mesh.getElements(dim,entity)
+            elementtags = convert(Vector{Vector{Int64}}, elementtags)[1]
+            for elementtag in elementtags
+                push!(cellsetelements, findfirst(isequal(elementtag), global_elementtags))
+            end
+        end
+        cellsets[name] = cellsetelements
+    end
+    return cellsets
+end
+
+function saved_file_to_grid(filename::String; domain="")
+    gmsh.initialize()
+    gmsh.open(filename)
+    fileextension = filename[findlast(isequal('.'),filename):end]
+    dim = Int64(gmsh.model.getDimension()) #dont ask..
+    facedim = dim-1
+
+    if fileextension != ".msh"
+        gmsh.model.mesh.generate(dim)
+    end    
+
+    nodes = tonodes()
+    elements, gmsh_elementidx = toelements(dim) 
+    boundarydict = toboundary(facedim)
+    facesets = tofacesets(boundarydict,elements)
+    cellsets = tocellsets(dim, gmsh_elementidx)
+    gmsh.finalize()
+
+    if !isempty(domain)
+        domaincellset = cellsets[domain]
+        elements = elements[collect(domaincellset)]
+    end
+        
+    return Grid(elements,nodes,facesets=facesets,cellsets=cellsets)
+end
+
 export gmsh
-export getnodes, getelements, getboundary, getfaceset
+export tonodes, toelements, toboundary, tofacesets, tocellsets, saved_file_to_grid
 
 end
