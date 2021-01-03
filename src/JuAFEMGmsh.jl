008938fb05c90bfb1ae851c0ec0ed1373cd31408
Maximilian Köhler <maximilian.koehler@ruhr-uni-bochum.de>
2021-01-03 13:46:11 +0100

    facesets work, cellsets and testing left

--- src/JuAFEMGmsh.jl
+++ src/JuAFEMGmsh.jl
@@ -15,15 +15,15 @@ const gmshtojuafemcell = Dict("Line 2" => Line,
                               "Hexahedron 20" => QuadraticHexahedron)
 
 function getnodes()
-    nodeid, nodes = JuAFEMGmsh.gmsh.model.mesh.getNodes()
-    dim = Int64(JuAFEMGmsh.gmsh.model.getDimension()) #Int64 otherwise julia crashes
+    nodeid, nodes = gmsh.model.mesh.getNodes()
+    dim = Int64(gmsh.model.getDimension()) #Int64 otherwise julia crashes
     return [Node(Vec{dim}(nodes[i:i+(dim-1)])) for i in 1:3:length(nodes)]
 end
 
 function getelements(dim)
-    elementtypes, elementtags, nodetags = JuAFEMGmsh.gmsh.model.mesh.getElements(dim)
+    elementtypes, elementtags, nodetags = gmsh.model.mesh.getElements(dim)
     @assert length(elementtypes)==1 "only one element type per mesh is supported"
-    elementname, dim, order, numnodes, localnodecoord, numprimarynodes = JuAFEMGmsh.gmsh.model.mesh.getElementProperties(elementtypes[1]) 
+    elementname, dim, order, numnodes, localnodecoord, numprimarynodes = gmsh.model.mesh.getElementProperties(elementtypes[1]) 
     nodetags = convert(Array{Array{Int64,1},1}, nodetags)[1]
     juafemcell = gmshtojuafemcell[elementname]
     elements = [juafemcell(Tuple(nodetags[i:i+(numnodes-1)])) for i in 1:numnodes:length(nodetags)]
@@ -36,16 +36,38 @@ function getboundary(dim)
     for (tag,boundary) in enumerate(boundaries)
         physicaltag = boundary[2]
         name = gmsh.model.getPhysicalName(dim, physicaltag)
-        boundarytypes, boundarytags, boundarynodetags = gmsh.model.mesh.getElements(dim, tag)
-        elementname, dim, order, numnodes, localnodecoord, numprimarynodes = JuAFEMGmsh.gmsh.model.mesh.getElementProperties(boundarytypes[1]) 
-        boundarynodetags = convert(Array{Array{Int64,1},1}, boundarynodetags)[1]
-        boundaryconnectivity = [Tuple(boundarynodetags[i:i+(numnodes-1)]) for i in 1:numnodes:length(boundarynodetags)]
+        boundaryentities = gmsh.model.getEntitiesForPhysicalGroup(dim, physicaltag)
+        boundaryconnectivity = Tuple[]
+        for entity in boundaryentities
+            boundarytypes, boundarytags, boundarynodetags = gmsh.model.mesh.getElements(dim, entity)
+            _, _, _, numnodes, _, _= gmsh.model.mesh.getElementProperties(boundarytypes[1]) 
+            boundarynodetags = convert(Array{Array{Int64,1},1}, boundarynodetags)[1]
+            append!(boundaryconnectivity,[Tuple(boundarynodetags[i:i+(numnodes-1)]) for i in 1:numnodes:length(boundarynodetags)])
+        end
         boundarydict[name] = boundaryconnectivity
     end 
     return boundarydict
 end
 
+function getfaceset(boundarydict::Dict{String,Vector}, elements::Vector{<:JuAFEM.AbstractCell})
+    faces = JuAFEM.faces.(elements)
+    facesets = Dict{String,Set{Tuple{Int64,Int64}}}()
+    for (boundaryname, boundaryfaces) in boundarydict
+        facesettuple = Set{Tuple{Int64,Int64}}()
+        for boundaryface in boundaryfaces
+            for (eleidx, elefaces) in enumerate(faces)
+                if boundaryface in elefaces
+                localface = findfirst(x->x==boundaryface, elefaces) 
+                push!(facesettuple, (eleidx, localface))
+                end
+            end
+        end
+        facesets[boundaryname] = facesettuple
+    end
+    return facesets
+end
+
 export gmsh
-export getnodes, getelements, getboundary
+export getnodes, getelements, getboundary, getfaceset
 
 end
--- test/devtests.jl
+++ test/devtests.jl
@@ -1,4 +1,5 @@
 using JuAFEMGmsh
+using JuAFEM, SparseArrays
 
 gmsh.initialize()
 gmsh.option.setNumber("General.Terminal", 1)
@@ -14,13 +15,80 @@ gmsh.model.geo.addLine(3, 4, 3)
 gmsh.model.geo.addLine(4, 1, 4)
 gmsh.model.geo.addCurveLoop([4, 1, -2, 3], 1)
 gmsh.model.geo.addPlaneSurface([1], 1)
-gmsh.model.addPhysicalGroup(0, [1, 2], 1)
+gmsh.model.geo.synchronize()
 gmsh.model.addPhysicalGroup(1, [1, 2, 4], 5)
 gmsh.model.setPhysicalName(1,5,"My Boundary")
-gmsh.model.addPhysicalGroup(2, [1], 6)
-gmsh.model.setPhysicalName(2, 6, "My Domain")
-gmsh.model.geo.synchronize()
+ps = gmsh.model.addPhysicalGroup(2, [1])
+gmsh.model.setPhysicalName(2, ps, "My Domain")
 gmsh.model.mesh.generate(2)
 nodes = getnodes()
 elements = getelements(2)
 boundarydict = getboundary(1)
+faceset = getfaceset(boundarydict, elements)
+
+grid = Grid(elements,nodes,facesets=faceset)
+
+dim = 2
+ip = Lagrange{dim, RefTetrahedron, 1}()
+qr = QuadratureRule{dim, RefTetrahedron}(2)
+cellvalues = CellScalarValues(qr, ip);
+
+dh = DofHandler(grid)
+push!(dh, :u, 1)
+close!(dh);
+
+K = create_sparsity_pattern(dh);
+
+ch = ConstraintHandler(dh);
+
+∂Ω = union(JuAFEM.getfaceset.((grid, ), ["My Boundary"])...);
+
+dbc = Dirichlet(:u, ∂Ω, (x, t) -> 0)
+add!(ch, dbc);
+
+close!(ch)
+update!(ch, 0.0);
+
+function doassemble(cellvalues::CellScalarValues{dim}, K::SparseMatrixCSC, dh::DofHandler) where {dim}
+
+    n_basefuncs = getnbasefunctions(cellvalues)
+    Ke = zeros(n_basefuncs, n_basefuncs)
+    fe = zeros(n_basefuncs)
+
+    f = zeros(ndofs(dh))
+    assembler = start_assemble(K, f)
+
+    @inbounds for cell in CellIterator(dh)
+
+        fill!(Ke, 0)
+        fill!(fe, 0)
+
+        reinit!(cellvalues, cell)
+
+        for q_point in 1:getnquadpoints(cellvalues)
+            dΩ = getdetJdV(cellvalues, q_point)
+
+            for i in 1:n_basefuncs
+                v  = shape_value(cellvalues, q_point, i)
+                ∇v = shape_gradient(cellvalues, q_point, i)
+                fe[i] += v * dΩ
+                for j in 1:n_basefuncs
+                    ∇u = shape_gradient(cellvalues, q_point, j)
+                    Ke[i, j] += (∇v ⋅ ∇u) * dΩ
+                end
+            end
+        end
+
+        assemble!(assembler, celldofs(cell), fe, Ke)
+    end
+    return K, f
+end
+
+K, f = doassemble(cellvalues, K, dh);
+
+apply!(K, f, ch)
+u = K \ f;
+
+vtk_grid("heat_equation", dh) do vtk
+    vtk_point_data(vtk, dh, u)
+end
